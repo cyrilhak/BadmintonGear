4722511567d67755db6870b80987cd18a7b5bd0e
Cyril Hak <cyril.hak@gmail.com>
2011-12-07 16:19:10 +0100

    initial commit

---
+++ includes/configure.php
@@ -0,0 +1,31 @@
+<?php
+  define('HTTP_SERVER', 'http://10.131.6.58');
+  define('HTTPS_SERVER', 'http://10.131.6.58');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/catalog/');
+  define('HTTPS_COOKIE_PATH', '/catalog/');
+  define('DIR_WS_HTTP_CATALOG', '/catalog/');
+  define('DIR_WS_HTTPS_CATALOG', '/catalog/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_BOXES', DIR_WS_INCLUDES . 'boxes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/home/php/public_html/catalog/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', '10.131.6.58');
+  define('DB_SERVER_USERNAME', 'shinyredapples');
+  define('DB_SERVER_PASSWORD', 'securepassword');
+  define('DB_DATABASE', 'oscommerce');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+?>

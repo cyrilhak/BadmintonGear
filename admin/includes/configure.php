64ee17c918568eae906b5b6217a4ceb69e6f6b4a
Cyril <chak@haarlem.nl>
2012-03-23 17:24:41 +0100

    directory changed from catalog to badmintongear

--- admin/includes/configure.php
+++ admin/includes/configure.php
@@ -1,13 +1,27 @@
 <?php
-  define('HTTP_SERVER', 'http://192.168.0.100');
-  define('HTTP_CATALOG_SERVER', 'http://192.168.0.100');
-  define('HTTPS_CATALOG_SERVER', 'http://192.168.0.100');
-  define('ENABLE_SSL_CATALOG', 'false');
-  define('DIR_FS_DOCUMENT_ROOT', '/home/badmintongear/public_html/catalog/');
-  define('DIR_WS_ADMIN', '/catalog/admin/');
-  define('DIR_FS_ADMIN', '/home/badmintongear/public_html/catalog/admin/');
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+// define our webserver variables
+// FS = Filesystem (physical)
+// WS = Webserver (virtual)
+  define('HTTP_SERVER', ''); // eg, http://localhost or - https://localhost should not be NULL for productive servers
+  define('HTTP_CATALOG_SERVER', '');
+  define('HTTPS_CATALOG_SERVER', '');
+  define('ENABLE_SSL_CATALOG', 'false'); // secure webserver for catalog module
+  define('DIR_FS_DOCUMENT_ROOT', $DOCUMENT_ROOT); // where your pages are located on the server. if $DOCUMENT_ROOT doesnt suit you, replace with your local path. (eg, /usr/local/apache/htdocs)
+  define('DIR_WS_ADMIN', '/admin/');
+  define('DIR_FS_ADMIN', DIR_FS_DOCUMENT_ROOT . DIR_WS_ADMIN);
   define('DIR_WS_CATALOG', '/catalog/');
-  define('DIR_FS_CATALOG', '/home/badmintongear/public_html/catalog/');
+  define('DIR_FS_CATALOG', DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG);
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
@@ -25,10 +39,11 @@
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
 
-  define('DB_SERVER', '192.168.0.100');
-  define('DB_SERVER_USERNAME', 'shinyredapples');
-  define('DB_SERVER_PASSWORD', 'securepassword');
-  define('DB_DATABASE', 'oscommerce');
+// define our database connection
+  define('DB_SERVER', '');
+  define('DB_SERVER_USERNAME', 'mysql');
+  define('DB_SERVER_PASSWORD', '');
+  define('DB_DATABASE', 'osCommerce');
   define('USE_PCONNECT', 'false');
-  define('STORE_SESSIONS', 'mysql');
+  define('STORE_SESSIONS', '');
 ?>
--- includes/configure.php
+++ includes/configure.php
@@ -1,13 +1,27 @@
 <?php
-  define('HTTP_SERVER', 'http://192.168.0.100');
-  define('HTTPS_SERVER', 'http://192.168.0.100');
-  define('ENABLE_SSL', false);
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2003 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+// Define the webserver and path parameters
+// * DIR_FS_* = Filesystem directories (local/physical)
+// * DIR_WS_* = Webserver directories (virtual/URL)
+  define('HTTP_SERVER', ''); // eg, http://localhost - should not be empty for productive servers
+  define('HTTPS_SERVER', ''); // eg, https://localhost - should not be empty for productive servers
+  define('ENABLE_SSL', false); // secure webserver for checkout procedure?
   define('HTTP_COOKIE_DOMAIN', '');
   define('HTTPS_COOKIE_DOMAIN', '');
-  define('HTTP_COOKIE_PATH', '/catalog/');
-  define('HTTPS_COOKIE_PATH', '/catalog/');
-  define('DIR_WS_HTTP_CATALOG', '/catalog/');
-  define('DIR_WS_HTTPS_CATALOG', '/catalog/');
+  define('HTTP_COOKIE_PATH', '');
+  define('HTTPS_COOKIE_PATH', '');
+  define('DIR_WS_HTTP_CATALOG', '');
+  define('DIR_WS_HTTPS_CATALOG', '');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_INCLUDES', 'includes/');
@@ -18,14 +32,15 @@
   define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
 
   define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
-  define('DIR_FS_CATALOG', '/home/badmintongear/public_html/catalog/');
+  define('DIR_FS_CATALOG', dirname($HTTP_SERVER_VARS['SCRIPT_FILENAME']) . '/');
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
 
-  define('DB_SERVER', '192.168.0.100');
-  define('DB_SERVER_USERNAME', 'shinyredapples');
-  define('DB_SERVER_PASSWORD', 'securepassword');
-  define('DB_DATABASE', 'oscommerce');
-  define('USE_PCONNECT', 'false');
-  define('STORE_SESSIONS', 'mysql');
+// define our database connection
+  define('DB_SERVER', ''); // eg, localhost - should not be empty for productive servers
+  define('DB_SERVER_USERNAME', '');
+  define('DB_SERVER_PASSWORD', '');
+  define('DB_DATABASE', 'osCommerce');
+  define('USE_PCONNECT', 'false'); // use persistent connections?
+  define('STORE_SESSIONS', ''); // leave empty '' for default handler or set to 'mysql'
 ?>
